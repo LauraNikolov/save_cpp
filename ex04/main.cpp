03f7177d72e2e87b719a73c65936de7c6f567b95
Laura Nicoloff <lnicolof@made-f0Ar8s5.clusters.42paris.fr>
2024-11-08 14:17:19 +0100

    ok ok

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,7 +3,11 @@
 
 void search_and_replace(std::string line, std::string s1, std::string s2)
 {
-    if(line == )
+    size_t pos = line.find(s1);
+    //if(pos == 0)
+    {
+        std::cout << line << std::endl;
+    }
 }
 
 int main(int argc, char **argv)
@@ -11,13 +15,12 @@ int main(int argc, char **argv)
     if(argc != 4)
         return(std::cout << "Error: Wrong argument\n", 0);
     std::ifstream file(argv[1]);
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
     if(!file)
         return(std::cerr << "Error: can't open file\n", 0);
     std::string line;
     while(getline(file, line))
-    {
-        std::cout << line << std::endl;
-        search_and_replace(line, std::string s1(argv[2]), std::string s2(argv[3]));
-    }
-    file.close;
+        search_and_replace(line, s1, s2);
+    file.close();
 }
