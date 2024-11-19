fb35110541c0491bfaffddbb73c69cdfbefd3f13
Laura Nicoloff <lnicolof@made-f0Ar12s1.clusters.42paris.fr>
2024-11-19 19:17:37 +0100

    update

---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicolof <lnicolof@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 13:17:52 by lnicolof          #+#    #+#             */
+/*   Updated: 2024/11/19 16:42:06 by lnicolof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+    #define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+
+class Fixed {
+    
+    public:
+        //constructuer de copie
+        Fixed(); 
+        //constructeur de recopie
+        Fixed(const Fixed &); 
+        Fixed(int const raw);
+        Fixed(float const raw);
+        //Operateur d'affectation
+        Fixed& operator=(Fixed const &other);
+        ~Fixed(); 
+        // Retourne la valeur brute du nombre en virgule fixe
+        int getRawBits() const;
+        // Initialise la valeur brute du nombre en virgule fixe
+        void setRawBits(int const raw);    
+        //convertit la valeur en virgule fixe en nombre entier
+        int toInt(void) const ;
+        //convertit la velur en virgule fixe en virgule flottante
+        float toFloat(void) const ;
+    
+    private:
+        int _value; //valeur fixe
+        static const int _fractionnalBits = 8; //nombre de bits pour la partie fractionnaire
+} ;
+
+
+// Surcharge de l'opérateur d'insertion (<<) pour afficher le Fixed
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
---
+++ cpp_02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicolof <lnicolof@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 18:03:00 by lnicolof          #+#    #+#             */
+/*   Updated: 2024/11/19 16:40:15 by lnicolof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed const b( 10 );
+Fixed const c( 42.42f );
+Fixed const d( b );
+a = Fixed( 1234.4321f );
+std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+std::cout << "c is " << c << std::endl;
+std::cout << "d is " << d << std::endl;
+std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+return 0;
+}
