d97401888e79a27da3367d81d4e0dfd496819476
Laura Nicoloff <lnicolof@paul-f4Br5s6.clusters.42paris.fr>
2024-12-12 19:23:52 +0100

    materiasource cpp

---
+++ cpp_04/ex03/MateriaSource.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicolof <lnicolof@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 19:19:05 by lnicolof          #+#    #+#             */
+/*   Updated: 2024/12/12 19:23:36 by lnicolof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource() {
+    
+}
+
+/* Copie la Materia passée en paramètre et la stocke en mémoire afin de la cloner
+plus tard. Tout comme le Character, la MateriaSource peut contenir 4 Materias
+maximum. Ces dernières ne sont pas forcément uniques */
+
+void MateriaSource::learnMateria(AMateria *other) {
+}
--- cpp_04/ex03/MateriaSource.hpp
+++ cpp_04/ex03/MateriaSource.hpp
@@ -6,14 +6,15 @@
 /*   By: lnicolof <lnicolof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 19:13:19 by lnicolof          #+#    #+#             */
-/*   Updated: 2024/12/12 19:17:49 by lnicolof         ###   ########.fr       */
+/*   Updated: 2024/12/12 19:21:36 by lnicolof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATERIASOURCE_HPP
     #define MATERIASOURCE_HPP
 
-#include "IMateriaSource.cpp"
+#include "IMateriaSource.hpp"
+#include "AMateria.hpp"
 
 class MateriaSource : public IMateriaSource {
     MateriaSource();
@@ -22,5 +23,9 @@ class MateriaSource : public IMateriaSource {
     virtual ~MateriaSource();
     void learnMateria(AMateria*);
     AMateria* CreateMateria(std::string const & type);
+
+    private:
+        AMateria *inventory[4];
+    
 }
 #endif
